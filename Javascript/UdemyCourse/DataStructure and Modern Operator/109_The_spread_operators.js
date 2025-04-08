2f10a6cd841d175304a1102bc35989141013e29f
Tran Kien Phong <kienphongtran2003@gmail.com>
2025-04-08 18:57:16 +0700

    spread operator

--- Javascript/UdemyCourse/DataStructure and Modern Operator/109_The_spread_operators.js
+++ Javascript/UdemyCourse/DataStructure and Modern Operator/109_The_spread_operators.js
@@ -53,6 +53,42 @@ const restaurant = {
 //?Spread Operators
 const arr = [7,8,9];
 const spreadArr = [1,2,...arr]; // Spread operator
+
 console.log(spreadArr); // [1,2,7,8,9]
 
+console.log(...spreadArr); // 1 2 7 8 9
 
+const newMenu = [...restaurant.mainMenu, 'Gnocchi']; // Spread operator to add 'Gnocchi' to the mainMenu array
+console.log(newMenu); // ['Pizza', 'Pasta', 'Risotto', 'Gnocchi']
+
+
+//? Copy Array
+const mainMenuCopy = [...restaurant.mainMenu]; // Spread operator to copy the mainMenu array
+console.log(mainMenuCopy); // ['Pizza', 'Pasta', 'Risotto']
+
+
+//? Join 2 arrays
+
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu]; // Spread operator to join the starterMenu and mainMenu arrays
+console.log(menu);
+
+
+//? Iterables :  arrays, strings, maps, sets. NOT objects
+const str = 'Jonas';
+const letters = [...str, ' ', 'S.']; // Spread operator to convert the string into an array of letters
+console.log(letters); // ['J', 'o', 'n', 'a', 's', ' ', 'S.']
+
+
+// const ingredients = [ prompt(' let\'s make pasta! Ingredient 1?'), prompt('Ingredient 2?'), prompt('Ingredient 3?') ];
+// console.log(ingredients); // ['Ingredient 1', 'Ingredient 2', 'Ingredient 3']
+
+// restaurant.ordersPasta(...ingredients); // Spread operator to pass the ingredients array as arguments to the ordersPasta function
+
+//? Object 
+const newRestaurant = {...restaurant, founder: 'Giuseppe Rossi', foundedIn: 1998}; // Spread operator to create a new object with the properties of the restaurant object and add new properties
+console.log(newRestaurant); // {name: 'Classico Italiano', location: 'Via Angelo Tavanti 23, Firenze, Italy', categories: Array(4), starterMenu: Array(4), mainMenu: Array(3), …}
+
+const restaurantCopy = {...restaurant}; // Spread operator to create a copy of the restaurant object
+restaurantCopy.name = 'Ristorante Roma'; // Change the name of the restaurantCopy object
+console.log(restaurantCopy.name);
+console.log(restaurant.name);
